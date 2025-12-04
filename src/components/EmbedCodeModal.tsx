b49a3586c2e7f09748fee023c19d9679f7c49d85
unknown <fadahunsi.miracle@gmail.com>
2025-12-04 21:40:09 +0100

    FIX: New Changes Made

--- src/components/EmbedCodeModal.tsx
+++ src/components/EmbedCodeModal.tsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useMemo } from 'react';
 import { Modal } from './ui/Modal';
 import { Button } from './ui/Button';
@@ -58,7 +57,7 @@ export const EmbedCodeModal: React.FC<EmbedCodeModalProps> = ({ isOpen, onClose,
             container.style.left = '0';
             container.style.top = '0';
             iframe.style.width = '100%';
-            iframe.style.height = '100dvh'; // Use 100dvh to ignore address bar
+            iframe.style.height = '100%';
             iframe.style.borderRadius = '0';
         } else {
             // Floating widget on desktop or closed state
